42cd4d4aae7acbea8f1f65b88da264555b86875f
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-09-30 12:56:23 +0500

    Complete Ex 1.14: Anecdotes Step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -2,37 +2,57 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+  const data = [
+    { anecdote: 'If it hurts, do it more often.', votes: 0 },
+    { anecdote: 'Adding manpower to a late software project makes it later!', votes: 0 },
+    { anecdote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
+    { anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0 },
+    { anecdote: 'Premature optimization is the root of all evil.', votes: 0 },
+    { anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0 },
+    { anecdote: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', votes: 0 },
+    { anecdote: 'The only way to go fast, is to go well.', votes: 0 }
   ]
 
-  function changeAnecdote() {
-    const index = Math.floor(Math.random() * anecdotes.length);
-    setSelected(index);
+  function setNextAnecdote() {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
   }
 
-  function updateVote() {
-    const copy = [...votes];
-    copy[selected]++;
-    setVotes(copy);
+  function updateVotes() {
+    const copy = [...anecdotes];
+
+    copy[selected].votes++;
+    setAnecdotes(copy);
+  }
+
+  function getMostVoted() {
+    let mostVoted = 0;
+    let greatestVote = 0;
+
+    anecdotes.forEach((anecdote, index) => {
+      if (anecdote.votes > greatestVote) {
+        mostVoted = index;
+        greatestVote = anecdote.votes;
+      }
+    });
+
+    return mostVoted;
   }
 
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [anecdotes, setAnecdotes] = useState(data);
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>Has {votes[selected]} votes</p>
-      <button onClick={updateVote}>Vote</button>
-      <button onClick={changeAnecdote}>Next Anecdote</button>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected].anecdote}</p>
+      <p>Has {anecdotes[selected].votes} votes</p>
+      <button onClick={updateVotes}>Vote</button>
+      <button onClick={setNextAnecdote}>Next Anecdote</button>
+
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[getMostVoted()].anecdote}</p>
+      <p>Has {anecdotes[getMostVoted()].votes} votes</p>
     </div>
   )
 }
