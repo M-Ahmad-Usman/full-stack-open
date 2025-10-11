5ee521dad5a90339310209af149e186cf490e105
Muhammad Ahmad <developer.ahmad@proton.me>
2025-10-12 00:13:52 +0500

    Complete Exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,49 +1,5 @@
 
-const Header = (props) => {
-
-	const { course } = props
-
-	return <h2>{course}</h2>
-}
-
-const Part = (props) => {
-
-	const { name, exercises } = props
-
-	return <p>{name} {exercises}</p>
-}
-
-const Content = (props) => {
-
-	const { parts } = props;
-
-	return parts
-		.map(
-			({ id, name, exercises }) =>
-				<Part key={id} name={name} exercises={exercises}></Part>
-		)
-}
-
-const Total = (props) => {
-
-	const { parts } = props;
-	const totalExercises = parts.reduce((accum, curr) => accum + curr.exercises, 0);
-
-	return <p><b>total of {totalExercises} exercises </b></p>
-}
-
-const Course = (props) => {
-	const { course } = props
-
-	return (
-		<>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</>
-	)
-
-}
+import Course from "./components/Course"
 
 const App = () => {
 
