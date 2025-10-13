87994109f69cf59a8898133e02a2e1966a3f8cfe
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-10-13 18:30:24 +0500

    Complete Exercise 2.6

--- part2/the_phonebook/src/App.jsx
+++ part2/the_phonebook/src/App.jsx
@@ -1,26 +1,40 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas' }
+	])
+
+	const [newName, setNewName] = useState('')
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		if (newName.trim() === '') return;
+
+		setPersons(persons.concat({ name: newName }));
+		setNewName("");
+	}
+
+	return (
+
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+
+			{persons.map(person => <span key={person.name}>{person.name}<br /></span>)}
+			...
+		</div>
+	)
 }
 
 export default App
