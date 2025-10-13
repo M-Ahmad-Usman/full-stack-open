e726ff80d37b98cb33725fe5f66b50a3ebee18fb
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-10-13 19:15:08 +0500

    Complete Exercise 2.9
    Add number field in for persons. Add robust error handling.

--- part2/the_phonebook/src/App.jsx
+++ part2/the_phonebook/src/App.jsx
@@ -3,24 +3,46 @@ import { useState } from 'react'
 const App = () => {
 
 	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas' }
+		{ name: 'Arto Hellas', number: "234-3242-14" }
 	])
 
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 
 	function handleSubmit(event) {
-		event.preventDefault();
+		event.preventDefault()
 
-		if (newName.trim() === '') return;
-
-		// If person with submitted name already exists
-		if (persons.find(person => person.name === newName) !== undefined) {
-			alert(`${newName} is already added to the phonebook.`)
-			return;
+		if (newName.trim() === '') {
+			alert("Person's name is required!")
+			return
 		}
 
-		setPersons(persons.concat({ name: newName.trim() }));
-		setNewName("");
+		if (newNumber.trim() === '') {
+			alert(`Phone Number is required for ${newName}`)
+			return
+		}
+
+		// If person with submitted number already exists
+		const existingPerson = persons.find(person => person.number === newNumber);
+		if (existingPerson !== undefined) {
+			alert(`${newNumber} is already added to the phonebook with name ${existingPerson.name}.`)
+			return
+		}
+
+		// If the number contains invalid characters: allow only digits and hyphens
+		if (/^[0-9\s-]+$/.test(newNumber) === false) {
+			alert('Only digits (0-9) and "-" are allowed for number.')
+			return
+		}
+
+		const newPerson = {
+			name: newName.trim(),
+			number: newNumber.trim()
+		}
+
+		setPersons(persons.concat(newPerson))
+		setNewName('')
+		setNewNumber('')
 	}
 
 	return (
@@ -32,12 +54,16 @@ const App = () => {
 					name: <input value={newName} onChange={e => setNewName(e.target.value)} />
 				</div>
 				<div>
+					number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/>
+				</div>
+				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
 
-			{persons.map(person => <span key={person.name}>{person.name}<br /></span>)}
+			{persons
+				.map(person => <span key={person.number}>{person.name} {person.number}<br /></span>)}
 			...
 		</div>
 	)
