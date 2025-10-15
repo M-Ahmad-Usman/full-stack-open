6388c24987c4a5777c7eb26be66afd5713f9a6ea
Muhammad Ahmad <developer.ahmad@proton.me>
2025-10-15 21:28:52 +0500

    Use axios & useEffect to fetch & render initial data

--- part2/the_phonebook/src/App.jsx
+++ part2/the_phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
@@ -6,17 +7,18 @@ import Persons from "./components/Persons"
 
 const App = () => {
 
-	const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
+	const [persons, setPersons] = useState([])
 
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 
 	const [searchText, setSearchText] = useState('')
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => setPersons(response.data))
+	}, [])
 
 	function handleSubmit(event) {
 		event.preventDefault()
@@ -58,22 +60,22 @@ const App = () => {
 
 		<div>
 			<h2>Phonebook</h2>
-			<Filter 
+			<Filter
 				searchText={searchText}
-				setSearchText={setSearchText}	
+				setSearchText={setSearchText}
 			/>
 
 			<h2>add a new</h2>
-			<PersonForm 
+			<PersonForm
 				newName={newName}
 				setNewName={setNewName}
 				newNumber={newNumber}
 				setNewNumber={setNewNumber}
-				handleSubmit={handleSubmit} 
+				handleSubmit={handleSubmit}
 			/>
 
 			<h2>Numbers</h2>
-			<Persons 
+			<Persons
 				persons={persons}
 				searchText={searchText}
 			/>
