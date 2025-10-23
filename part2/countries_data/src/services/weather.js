91a12efce3d0e917841646309d5b55b73d5d11c9
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-10-24 00:20:02 +0500

    Add weather service to handle weather http requests

---
+++ part2/countries_data/src/services/weather.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const baseURL = 'https://api.openweathermap.org/data/2.5'
+const baseIconURL = 'https://openweathermap.org/img/wn'
+
+const apiKey = import.meta.env.VITE_OPEN_WEATHER_API
+
+const getCityWeather = (countryName) => {
+    
+    countryName = countryName.toLowerCase();
+
+    return axios
+        .get(`${baseURL}/weather?q=${countryName}&units=metric&appid=${apiKey}`)
+        .then(res => res.data)
+}
+
+const getIconURL = (iconCode) => {
+    console.log(iconCode)
+    return `${baseIconURL}/${iconCode}@2x.png`
+}
+
+export default { getCityWeather, getIconURL }
