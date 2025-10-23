b7da54a06910bf61025fffc815dbc428c3ec0c85
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-10-24 00:21:41 +0500

    Use weather service to display capital's weather

--- part2/countries_data/src/components/Country.jsx
+++ part2/countries_data/src/components/Country.jsx
@@ -1,7 +1,30 @@
 
+// React
+import { useState, useEffect } from 'react'
+// Services
+import weatherService from "../services/weather"
+
 const Country = props => {
 
 	const { country } = props
+	
+	const [ weatherData, setWeatherData ] = useState({})
+
+	useEffect(() => {
+
+		weatherService
+			.getCityWeather(country.capital[0])
+			.then(data => {
+				
+				const newWeatherData = {
+					temperature: data.main.temp,
+					iconURL: weatherService.getIconURL(data.weather[0].icon),
+					wind: data.wind.speed
+				}
+				setWeatherData(newWeatherData)
+				
+			}).catch(error => console.log(error))
+	}, [])
 
 	return (
 		<>
@@ -17,6 +40,11 @@ const Country = props => {
 			</ul>
 
 			<img src={country.flags.png} alt={country.flags.alt} />
+
+			<h2>Weather in {country.capital}</h2>
+			<p>Temperature: {weatherData.temperature} Celcius</p>
+			<img src={weatherData.iconURL} alt="" />
+			<p>Wind: {weatherData.wind} m/s</p>
 		</>
 	)
 }
