64daedb8e3c41077e864ff0588e2207f429d66a3
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-01 19:25:47 +0500

    Add validation for creating new person objects.

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -11,7 +11,11 @@ mongoose.connect(MONGODB_URI)
 
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String, 
+        minLength: 3,
+        required: true
+    },
     number: String
 })
 
