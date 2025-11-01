e46d450aa9340d08c01ab507e6351d42f7123e91
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-01 13:51:15 +0500

    Remove the in-memory notes array.

--- notes-app/server/index.js
+++ notes-app/server/index.js
@@ -5,24 +5,6 @@ const Note = require('./models/note')
 
 const app = express()
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
-
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
