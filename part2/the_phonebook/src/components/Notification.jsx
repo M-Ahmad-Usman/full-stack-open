366669c609bf581b74adae55759a01eec4455125
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-01 18:42:20 +0500

    Add local Styles and separate styling for errors.

--- part2/the_phonebook/src/components/Notification.jsx
+++ part2/the_phonebook/src/components/Notification.jsx
@@ -1,11 +1,21 @@
 
-const Notification = ({ message }) => {
+const Notification = ({ message, isError=false }) => {
     if (message.trim() === '') {
         return null
     }
 
+    const style = {
+        color: isError ? 'red' : 'green',
+        background: 'lightgrey',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    }
+
     return (
-        <div className='notification'>
+        <div style={style}>
             {message}
         </div>
     )
