a3544b77d723ecd3abec6b4783e5239f53b1bdba
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-07 22:45:18 +0500

    Fix all linting issues.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -9,146 +9,146 @@ const Person = require('./models/person')
 app.use(express.json())
 
 // Custom body token to log body
-morgan.token('body', (req, res) => {
-    return JSON.stringify(req.body)
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
 })
 
 app.use(
-    morgan(function (tokens, req, res) {
+  morgan(function (tokens, req, res) {
 
-        const tokensArray = [
-            tokens.method(req, res),
-            tokens.url(req, res),
-            tokens.status(req, res),
-            tokens.res(req, res, 'content-length'), '-',
-            tokens['response-time'](req, res), 'ms',
-        ]
+    const tokensArray = [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+    ]
 
-        // Log body only if method is POST
-        if (req.method === "POST" || req.method === "PUT") tokensArray.push(tokens.body(req, res))
+    // Log body only if method is POST
+    if (req.method === 'POST' || req.method === 'PUT') tokensArray.push(tokens.body(req, res))
 
-        // Return formated string of predefined tokens
-        return tokensArray.join(' ')
-    })
+    // Return formated string of predefined tokens
+    return tokensArray.join(' ')
+  })
 )
 app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response, next) => {
 
-    Person.find({})
-        .then(people => response.json(people))
-        .catch(next)
+  Person.find({})
+    .then(people => response.json(people))
+    .catch(next)
 })
 
 app.get('/info', (request, response, next) => {
 
-    Person.countDocuments({})
-        .then(length => {
-            const markup =
-                `<p>Phonebook has info of ${length} people</p>
+  Person.countDocuments({})
+    .then(length => {
+      const markup =
+        `<p>Phonebook has info of ${length} people</p>
             <p>${new Date().toString()}</p>`
 
-            response.send(markup)
-        })
-        .catch(next)
+      response.send(markup)
+    })
+    .catch(next)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const personId = request.params.id
+  const personId = request.params.id
 
-    Person.findById(personId)
-        .then(person => response.json(person))
-        .catch(next)
+  Person.findById(personId)
+    .then(person => response.json(person))
+    .catch(next)
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
 
-    const personId = request.params.id
+  const personId = request.params.id
 
-    Person.findByIdAndDelete(personId)
-        .then(deletedPerson => response.status(204).end())
-        .catch(next)
+  Person.findByIdAndDelete(personId)
+    .then(() => response.status(204).end())
+    .catch(next)
 
 })
 
 app.post('/api/persons', (request, response, next) => {
 
-    const contentTypeHeader = request.get('Content-Type')
+  const contentTypeHeader = request.get('Content-Type')
 
-    if (contentTypeHeader !== 'application/json')
-        return response.status(400).json({
-            error: 'Person data is expected in JSON format'
-        })
-
-    const { name, number } = request.body
-
-    if (!name || !number) return response.status(400).json({
-        error: "Person's name and number are required"
+  if (contentTypeHeader !== 'application/json')
+    return response.status(400).json({
+      error: 'Person data is expected in JSON format'
     })
 
-    Person.exists({ name: name })
-        .then(existingPersonId => {
-            if (existingPersonId !== null) {
-                return response.status(400).json({
-                    error: `Person ${name} already exists in the phonebook`
-                })
-            }
-            else {
-                const newPerson = new Person({
-                    name,
-                    number,
-                })
+  const { name, number } = request.body
 
-                newPerson.save()
-                    .then(newPerson => response.json(newPerson))
-                    .catch(next)
-            }
+  if (!name || !number) return response.status(400).json({
+    error: `Person's name and number are required`
+  })
+
+  Person.exists({ name: name })
+    .then(existingPersonId => {
+      if (existingPersonId !== null) {
+        return response.status(400).json({
+          error: `Person ${name} already exists in the phonebook`
         })
-        .catch(next)
+      }
+      else {
+        const newPerson = new Person({
+          name,
+          number,
+        })
+
+        newPerson.save()
+          .then(newPerson => response.json(newPerson))
+          .catch(next)
+      }
+    })
+    .catch(next)
 
 
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
 
-    const { number } = request.body
+  const { number } = request.body
 
-    if (!number) {
-        return response.status(400).json({
-            error: 'Phone number is required to update the resource'
+  if (!number) {
+    return response.status(400).json({
+      error: 'Phone number is required to update the resource'
+    })
+  }
+
+  const personId = request.params.id
+
+  Person.findByIdAndUpdate(
+    personId,
+    { number: number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).json({
+          error: 'Person not found with the specified id'
         })
-    }
-
-    const personId = request.params.id
-
-    Person.findByIdAndUpdate(
-        personId,
-        { number: number },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedPerson => {
-            if (updatedPerson) {
-                response.json(updatedPerson)
-            } else {
-                response.status(404).json({
-                    error: 'Person not found with the specified id'
-                })
-            }
-        })
-        .catch(next)
+      }
+    })
+    .catch(next)
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error)
+  console.log(error)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).send({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
@@ -156,5 +156,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -6,31 +6,31 @@ mongoose.set('strictQuery', false)
 
 console.log('Connecting to MongoDB')
 mongoose.connect(MONGODB_URI)
-    .then(() => console.log("Connected to MongoDB successfuly"))
-    .catch(err => console.log("Error connecting to MongoDB ", err.message))
+  .then(() => console.log('Connected to MongoDB successfuly'))
+  .catch(err => console.log('Error connecting to MongoDB ', err.message))
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String, 
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        validate: {
-            validator : (number) => /^\d{2,3}-\d{5,}$/.test(number),
-            message: () => `Valid number Format is: xx-xxxxxx... or xxx-xxxxx...`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (number) => /^\d{2,3}-\d{5,}$/.test(number),
+      message: () => 'Valid number Format is: xx-xxxxxx... or xxx-xxxxx...'
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
