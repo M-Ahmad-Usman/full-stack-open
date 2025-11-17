73a34a14a709e6c6d9683a6b19ab35b17f6b4dc2
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-17 21:59:18 +0500

    Configure app startup.

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,28 +1,8 @@
 
-const express = require('express')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const morgan = require('morgan')
 const logger = require('./utils/logger')
-const blogRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-
-const app = express()
-
-app.use(express.json())
-app.use(morgan(logger.morganFormatFunction))
-
-logger.info('Trying to connect to MongoDB...')
-
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-  .then(() => logger.info('Connected to MongoDB'))
-  .catch(e => logger.error(`Error Connecting to MongoDB: ${e}`))
-
-app.use('/api/blogs', blogRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+const app = require('./app')
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on PORT ${config.PORT}`)
 })
