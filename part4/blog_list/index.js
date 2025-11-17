95beefad3f8b9700f3eb1d27ffa3140096ca9dc0
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-17 21:09:40 +0500

    Require controller for blogs.

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,10 +1,10 @@
 
 const express = require('express')
-const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const morgan = require('./utils/logger')
 const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -17,26 +17,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
   .then( () => logger.info('Connected to MongoDB') )
   .catch( e => logger.error(`Error Connecting to MongoDB: ${e}`) )
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-
-  const { title, author, url } = request.body
-
-  if (!title) return response.status(400).json({ error: `Blog's title is required` })
-  if (!author) return response.status(400).json({ error: 'Author is required' })
-  if (!url) return response.status(400).json({ error: `Blog's url is required` })
-
-  const blog = new Blog({ title, author, url, likes: 0 })
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
