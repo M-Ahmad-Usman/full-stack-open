46cb91ee781332a67ac374972d34ac02e9eb89e1
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-18 16:33:04 +0500

    Write unit tests for totalLikes function.

---
+++ part4/blog_list/tests/totalLikes.test.js
@@ -0,0 +1,30 @@
+
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+
+const listHelper = require('../utils/list_helper')
+const blogData = require('./blog_data')
+
+describe('Total Likes', () => {
+
+  test('of empty list is zero', () => {
+    const totalLikes = listHelper.totalLikes(blogData.emptyBlogList)
+    assert.strictEqual(totalLikes, 0)
+  })
+
+  test('when list has only one blog equals to the likes of that', () => {
+
+    const listWithOneBlog = blogData.listWithOneBlog
+
+    const totalLikes = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(totalLikes, listWithOneBlog[0].likes)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const listWithMultipleBlogs = blogData.listWithMultipleBlogs
+    const TOTAL_LIKES = 36 // Total likes in the list of multiple blogs
+
+    const totalLikes = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(totalLikes, TOTAL_LIKES)
+  })
+})
