fafefbec0a0fc5ae524f8f72206682a0a66200d0
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-18 16:32:38 +0500

    Add function to count total likes in blogs.

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,9 +1,19 @@
+
 const dummy = (blogs) => {
   if (Array.isArray(blogs))
     return 1
   return 0
 }
 
-module.exports = {
-  dummy
+const totalLikes = (blogs) => {
+  if (!Array.isArray(blogs))
+    throw new Error('An array of blogs is expected.')
+
+  const reducer = (total, blog) => total + blog.likes
+
+  return blogs.reduce(reducer, 0)
 }
+
+const helperFunctions = { dummy, totalLikes }
+
+module.exports = helperFunctions
