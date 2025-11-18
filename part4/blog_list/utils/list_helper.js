637e53498e8209ccbb251b5d47660edd3d5acd66
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-19 00:17:47 +0500

    Add mostBlogs function to find author with most blogs.

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -23,11 +23,56 @@ const favoriteBlog = (blogs) => {
   else if (blogs.length === 1)
     return blogs[0]
 
-  const reducer = (favorite, current) => current.likes > favorite.likes ? current: favorite
+  const reducer = (favorite, current) => current.likes > favorite.likes ? current : favorite
 
   return blogs.reduce(reducer)
 }
 
-const helperFunctions = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogs) => {
+  if (!Array.isArray(blogs))
+    throw new Error('An array of blogs is expected.')
+
+  if (blogs.length === 0)
+    return null
+
+  // Reduces blogs array to the following object
+  // { authorName: numberOfBlogs, ... }
+  // Here authorName and numberOfBlogs both are values from a blog object of blogs array
+  const reducer1 = (accum, curr) => {
+
+    if (Object.hasOwn(accum, curr.author))
+      accum[curr.author]++
+    else
+      accum[curr.author] = 1
+
+    return accum
+  }
+  const authors = blogs.reduce(reducer1, {})
+
+  // Returns the [author, blogs] block with highest value of blogs
+  const reducer2 = (curr, next) => {
+
+    // curr = [author, blogs]
+    // next = [author, blogs]
+
+    const blogsOfCurrAuthor = curr[1]
+    const blogsOfNextAuthor = next[1]
+
+    if (blogsOfCurrAuthor < blogsOfNextAuthor)
+      curr = next
+
+    return curr
+  }
+  const [authorWithMostBlogs, numberOfBlogs] = Object.entries(authors).reduce(reducer2)
+
+  return { author: authorWithMostBlogs, blogs: numberOfBlogs }
+}
+
+const helperFunctions = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
+}
 
 module.exports = helperFunctions
