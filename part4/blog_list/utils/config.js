8391a309e22b0fed9b43d66d376187121808dddd
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-22 22:59:17 +0500

    Specify DBs based on execution environment.

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -1,4 +1,6 @@
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.MONGODB_URI_TEST
+  : process.env.MONGODB_URI_DEV
 
 module.exports = { MONGODB_URI, PORT }
