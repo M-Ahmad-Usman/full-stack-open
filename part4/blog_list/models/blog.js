48c5a6acf65d83f60f5f4fff9f785a240f15a573
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-23 03:23:45 +0500

    Override toJson to transform documents.
    Replace _id with string id and delete __v

--- part4/blog_list/models/blog.js
+++ part4/blog_list/models/blog.js
@@ -8,6 +8,16 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 })
 
+// Convert the ObjectId _id property into String id
+// Delete _id and __v properties
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 module.exports = Blog
