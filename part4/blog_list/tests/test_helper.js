b76937637d9dd3570aa610c455dfff893e565fd1
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-24 00:15:21 +0500

    Implement functions to fetch blogs and get invalid id.

--- part4/blog_list/tests/test_helper.js
+++ part4/blog_list/tests/test_helper.js
@@ -1,3 +1,6 @@
+
+const Blog = require('../models/blog')
+
 const removeProperty = (document, property) => {
   if (!property) return null
 
@@ -5,6 +8,14 @@ const removeProperty = (document, property) => {
   return document
 }
 
-const helper = { removeProperty }
+const blogsInDB = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+const nonExistingId = () => '6922f4397e0fe27d8c421713'
+
+
+const helper = { removeProperty, blogsInDB, nonExistingId }
 
 module.exports = helper
