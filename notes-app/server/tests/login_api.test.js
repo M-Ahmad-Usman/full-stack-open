a6f838be5fd007645cc73f5653649cb7142e8eef
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-29 16:34:40 +0500

    Write tests for login api.

---
+++ notes-app/server/tests/login_api.test.js
@@ -0,0 +1,63 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+
+const helper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app)
+
+const baseEndpoint = '/api/login'
+
+describe('Login API', () => {
+
+  describe(`POST ${baseEndpoint}`, () => {
+
+    beforeEach(async () => {
+      await helper.clearDatabase()
+    })
+
+    test('succeds with status 200 on valid credentials', async () => {
+
+      const newUser = {
+        username: 'Ahmad',
+        password: 'Ahmad123'
+      }
+
+      await helper.createUser(newUser)
+
+      const res = await api.post(baseEndpoint)
+        .send(newUser)
+        .expect(200)
+
+      assert(res.body.token)
+      assert.strictEqual(res.body.username, newUser.username)
+
+    })
+
+    test('fails with status 401 on invalid user credentials',async () => {
+
+      const nonExistingUser = {
+        username: 'non-existing name',
+        password: 'non-existing password'
+      }
+
+      await api.post(baseEndpoint)
+        .send(nonExistingUser)
+        .expect(401)
+
+    })
+
+    test('fails with status 400 if no credentials are provided', async () => {
+      await api.post(baseEndpoint).expect(400)
+    })
+
+  })
+
+})
+
+after(async () => {
+  await helper.clearDatabase()
+  await mongoose.connection.close()
+})
