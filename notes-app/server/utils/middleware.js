697648c0260c7c100ea5f5e300bad653da3a6ec0
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-29 14:34:02 +0500

    Add error handling for token expiration.

--- notes-app/server/utils/middleware.js
+++ notes-app/server/utils/middleware.js
@@ -26,6 +26,9 @@ const errorHandler = (error, request, response, next) => {
   else if (error.name === 'JsonWebTokenError')
     return response.status(401).json({ error: 'token invalid' })
 
+  else if (error.name === 'TokenExpiredError')
+    return response.status(401).json({ error: 'token expired' })
+
   next(error)
 }
 
