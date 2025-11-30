0938c80ee07005a91c8624d04246d00a1c89df10
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-11-30 22:36:38 +0500

    Attach Login router to the app.

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -9,6 +9,7 @@ const middleware = require('./utils/middleware')
 // Routers
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -23,6 +24,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
