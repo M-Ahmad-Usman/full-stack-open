f24b48113c4abb3de17386dee4b0108fb3947faf
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-02 23:10:33 +0500

    Refactor POST /api/blogs tests.

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -81,6 +81,89 @@ describe('Blog API', () => {
 
   })
 
+  describe('POST /api/blogs', () => {
+
+    test('fails with 401 if no authentication header', async () => {
+
+      const blog = helper.getRandomBlog()
+
+      const response = await api.post(baseEndpoint)
+        .send(blog)
+        .expect(401)
+
+      const requiredKeywords = ['jwt', 'token', 'access token', 'required', 'bearer scheme']
+
+      const error = response.body.error.toLowerCase()
+
+      assert(requiredKeywords.every(k => error.includes(k)))
+    })
+
+    test(`fails with status 401 if token doesn't has expected data`, async () => {
+      const blog = helper.getRandomBlog()
+      const tokenWithInvalidPayload = helper.generateValidToken()
+
+      const response = await api.post(baseEndpoint)
+        .send(blog)
+        .auth(tokenWithInvalidPayload, { type: 'bearer' })
+        .expect(401)
+
+      const error = response.body.error.toLowerCase()
+      const requiredKeywords = ['token', 'invalid']
+
+      assert(requiredKeywords.every(k => error.includes(k)))
+    })
+
+    test('fails with status 401 if jwt token itself is invalid', async () => {
+      const blog = helper.getRandomBlog()
+      const invalidToken = helper.generateInvalidToken()
+
+      const response = await api.post(baseEndpoint)
+        .send(blog)
+        .auth(invalidToken, { type: 'bearer' })
+        .expect(401)
+
+      const error = response.body.error.toLowerCase()
+      const requiredKeywords = ['token', 'missing', 'invalid']
+
+      assert(requiredKeywords.every(k => error.includes(k)))
+    })
+
+    test('fails with 401 if token is expired', async () => {
+      const blog = helper.getRandomBlog()
+      const user = await helper.createUser()
+
+      // Expires in 1ms second
+      // Use very small time. 1 second doesn't works
+      const expiredToken = helper.generateValidToken(user, '1ms')
+
+      const response = await api.post(baseEndpoint)
+        .send(blog)
+        .auth(expiredToken, { type: 'bearer' })
+        .expect(401)
+
+      const error = response.body.error.toLowerCase()
+      assert(error.includes('expired'))
+    })
+
+    test('succeeds with status 201 on valid data and token', async () => {
+      const blog = helper.getRandomBlog()
+      const user = await helper.createUser()
+
+      const token = helper.generateValidToken(user)
+
+      await api.post(baseEndpoint)
+        .send(blog)
+        .auth(token, { type: 'bearer' })
+        .expect(201)
+
+      const blogsInDB = await helper.getBlogsInDB()
+
+      // DB is cleared before every test so there will be only 1 blog
+      assert.strictEqual(blogsInDB.length, 1)
+    })
+
+  })
+
 })
 
 // After everything is done we have to close the mongoDB collection
