96b45656b5f2e99d330ab8b13af8b4bd4d869f9f
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-05 23:51:19 +0500

    Set user token to post notes.

--- notes-app/client/src/App.jsx
+++ notes-app/client/src/App.jsx
@@ -58,7 +58,7 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password })
-
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
