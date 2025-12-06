e61d23049f32db35d1292b0e646c1e93fde15425
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-06 16:39:50 +0500

    Add BlogForm to allow addition of new blogs.

---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,75 @@
+
+import { useState } from "react"
+import blogService from '../services/blogs'
+
+const BlogForm = (props) => {
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const { blogs, setBlogs } = props
+
+  const handleBlogSubmit = async (event) => {
+
+    const form = event.target
+
+    form.reportValidity()
+
+    event.preventDefault()
+
+    if (title.trim() === '' || author.trim() === '' || url.trim() === '') {
+      alert('title, author and url are required.')
+      return;
+    }
+
+    const newBlog = { title, author, url }
+
+    const createdBlog = await blogService.create(newBlog)
+    setBlogs(blogs.concat(createdBlog))
+  }
+
+  return (
+    <form onSubmit={handleBlogSubmit}>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="author">Author:</label>
+        <input
+          type="text"
+          name="author"
+          id="author"
+          required
+          value={author}
+          onChange={e => setAuthor(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="url">URL:</label>
+        <input
+          type="url"
+          name="url"
+          id="url"
+          required
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Add Blog</button>
+    </form>
+  )
+}
+
+export default BlogForm
