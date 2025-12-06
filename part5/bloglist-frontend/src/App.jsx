42a2373ce46d1a3678d14f12191facee7a13b780
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-06 14:49:47 +0500

    Remove login handling and its functionality.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -6,12 +6,9 @@ import LoginForm from './components/LoginForm'
 
 // Services
 import blogService from './services/blogs'
-import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -20,41 +17,12 @@ const App = () => {
     )
   }, [])
 
-  const handleLogin = async (event) => {
-
-    event.preventDefault()
-
-    setUsername(username.trim())
-    setPassword(password.trim())
-
-    // validations
-    if ((username === '' || password === '') || (username.length < 3 || password.length < 3)) {
-      alert('Both password and username must be 3 characters long.')
-      return
-    }
-
-    try {
-      const user = await loginService.login({ username, password })
-      setUser(user)
-    } catch (error) {
-      console.log(error)
-      alert('Invalid Credentials')
-    }
-
-  }
-
   // If not logged in, ask to login
   if (user === null)
     return (
       <div>
         <h2>Login to application</h2>
-        <LoginForm
-          username={username}
-          password={password}
-          setUsername={setUsername}
-          setPassword={setPassword}
-          handleLogin={handleLogin}
-        />
+        <LoginForm setUser={setUser} />
       </div>
     )
 
@@ -66,7 +34,6 @@ const App = () => {
       </div>
 
       <div>
-        <h2>blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
