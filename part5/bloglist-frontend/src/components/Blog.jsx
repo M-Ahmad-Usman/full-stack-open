48b4ef0e331f0864f677b8be2412d2b8ea1d528f
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-14 01:44:51 +0500

    Complete Exercise 5.7.
    Add button to toggle blog details.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  // Hide when details are visible
+  const hideWhenVisible = { display: showDetails ? 'none' : 'block' }
+  const showWhenVisible = { display: showDetails ? 'block' : 'none' }
+
+  const toggleVisbility = () => setShowDetails(!showDetails)
+  const buttonLabel = showDetails ? 'Hide' : 'View'
+
+   const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisbility}>{buttonLabel}</button>
+      </div>
+
+
+      <div style={showWhenVisible}>
+        <b>Title:</b> {blog.title} <button onClick={toggleVisbility}>{buttonLabel}</button> <br />
+        <b>Author</b> {blog.author} <br />
+        <b>URL</b> {blog.url} <br />
+        <b>Likes:</b> {blog.likes} <button>Like</button> <br />
+      </div>
+    </div>
+  )
+}
 
 export default Blog
