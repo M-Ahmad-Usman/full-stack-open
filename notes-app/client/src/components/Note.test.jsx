cbc0ae13eb90947e67ba3ed3f06fe5ec51593dac
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-23 15:14:37 +0500

    Add comments to explain methods used.

--- notes-app/client/src/components/Note.test.jsx
+++ notes-app/client/src/components/Note.test.jsx
@@ -9,6 +9,14 @@ test('renders content', () => {
 
   render(<Note note={note} />)
 
+  // getByText by default searches for an element that contains only the text provided as a parameter and nothing else.
+  // If an element has some additional text then getByText will fail to find it.
   const element = screen.getByText('Component testing is done with react-testing-library')
+  // Other options
+  // 1. We can provide an additional option like screen.getByText('text', { exact: false }) to perform a partial match.
+  // const element = screen.getByText('text', { exact: false })
+
+  // 2. We can use findByText. It is important to notice that, unlike the other ByText methods, findByText returns a promise!
+  // const element = await screen.findByText('text')
   expect(element).toBeDefined()
 })
