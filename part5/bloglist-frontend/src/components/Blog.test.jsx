64a0ff0ae5972dceeb1ab93177a31a013544819e
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-12-26 00:17:01 +0500

    Complete Exe. 5.14
    Write test to verify that blog's url and likes are shown when the component is expanded.

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog.jsx'
 import { expect, test } from 'vitest'
 
@@ -21,4 +22,26 @@ test("render's blog title and author by default", () => {
   // URL and likes should NOT be visible by default
   expect(screen.queryByText('https://reactpatterns.com/')).not.toBeInTheDocument()
   expect(screen.queryByText('7')).not.toBeInTheDocument()
+})
+
+test('all blog details are shown when component is expanded', async () => {
+  const user = { username: 'Ahmad' }
+
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user
+  }
+
+  render(<Blog blog={blog} user={user} />)
+
+  const event = userEvent.setup()
+  const button = screen.getByText('View')
+  await event.click(button)
+
+  expect(screen.getByText(blog.url, { exact: false })).toBeInTheDocument()
+  expect(screen.getByText(blog.likes, { exact: false })).toBeInTheDocument()
+
 })
