d26fa6ca419b272bb68885abaeb3c6c49ea086ae
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-01-24 15:24:25 +0500

    Attach testing endpoint only when server is in testing mode.

--- notes-app/server/app.js
+++ notes-app/server/app.js
@@ -31,6 +31,11 @@ app.use('/api/notes', notesRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 // app.use() without a path is the idiomatic Express way to handle 404s
 // because it naturally falls at the end of the middleware chain and
 // only executes when nothing else matched.
