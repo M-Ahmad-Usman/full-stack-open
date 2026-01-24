d0da5202cbe50dcde7aef22bffbe1c4a35434561
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-01-24 14:09:19 +0500

    Add test for testing the user login flow.

--- notes-app/testing/tests/note_app.spec.js
+++ notes-app/testing/tests/note_app.spec.js
@@ -3,12 +3,25 @@ const { test, expect, describe } = require('@playwright/test')
 describe('Note app', () => {
 
   test('front page can be opened', async ({ page }) => {
-    
+
     await page.goto('http://localhost:5173')
 
     const locator = page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await page.getByLabel('username').fill('alice_johnson')
+    await page.getByLabel('password').fill('password123')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Alice Johnson logged in')).toBeVisible()
   })
 
 })
