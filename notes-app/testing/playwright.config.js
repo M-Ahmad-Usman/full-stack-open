aa1931e7b235c65dfd3e588e68b32191eef18cf6
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-01-24 13:46:46 +0500

    Update timeout to 5 seconds since 3 seconds isn't working reliably in UI mode.

--- notes-app/testing/playwright.config.js
+++ notes-app/testing/playwright.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  /* Reduce the timeout to 3 seconds during development */
-  timeout: 3000,
+  /* Reduce the timeout to 5 seconds during development */
+  timeout: 5000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
