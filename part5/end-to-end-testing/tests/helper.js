b966d98845674b261772ad9d3eecbe90a28cf7be
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-01-26 19:04:01 +0500

    Add helper function to create blogs.

--- part5/end-to-end-testing/tests/helper.js
+++ part5/end-to-end-testing/tests/helper.js
@@ -9,6 +9,17 @@ const loginUser = async (page, user) => {
   await page.getByRole('button', { name: 'login' }).click()
 }
 
-const helper = { loginUser }
+const createBlog = async (page, blog) => {
+  await page.getByRole('button', { name: 'Create New Blog' }).click()
+
+  // fill in the form with blog data
+  await page.getByLabel('Title').fill(blog.title)
+  await page.getByLabel('Author').fill(blog.author)
+  await page.getByLabel('URL').fill(blog.URL)
+
+  await page.getByRole('button', { name: 'Add Blog' }).click()
+}
+
+const helper = { loginUser, createBlog }
 
 module.exports = helper
