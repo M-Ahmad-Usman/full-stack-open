bda3ae4a4139ef1c9670f6e7749316545ee3d8a6
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-01-26 10:35:02 +0500

    Complete Exe. 5.18
    Write tests for user login flow.

--- part5/end-to-end-testing/tests/bloglist_app.spec.js
+++ part5/end-to-end-testing/tests/bloglist_app.spec.js
@@ -1,13 +1,30 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const helper = require('./helper')
+
+const { loginUser } = helper
+
+// User for testing
+const USER1 = {
+  name: 'Matti Luukkainen',
+  username: 'mluukkai',
+  password: 'salainen'
+}
 
 describe('Blog app', () => {
 
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+
+    // Reset the database
+    await request.post('/api/testing/reset')
+
+    // Create user in the db
+    await request.post('/api/users', { data: USER1 })
+
     await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
-    
+
     const loginBtn = page.getByRole('button', { name: 'login' })
 
     await expect(loginBtn).toBeVisible()
@@ -17,6 +34,28 @@ describe('Blog app', () => {
     // Make sure that the login form is visible
     expect(page.getByLabel('username')).toBeVisible()
     expect(page.getByLabel('password')).toBeVisible()
-    
+
   })
+
+  describe('login', () => {
+
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginUser(page, USER1)
+
+      await expect(page.getByText(`${USER1.username} logged in`)).toBeVisible()
+
+      await expect(page.getByText('Invalid Credentials')).not.toBeVisible()
+
+    })
+
+    test('fails with incorrect credentials', async ({ page }) => {
+      await loginUser(page, { username: 'incorrect', password: 'incorrect'})
+
+      await expect(page.getByText(`${USER1.username} logged in`)).not.toBeVisible()
+
+      await expect(page.getByText('Invalid Credentials')).toBeVisible()
+    }) 
+
+  })
+
 })
