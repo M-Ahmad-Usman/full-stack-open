8403312f6a8ddf241b2f0fa0e51edc5b1a0b65f5
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-02-21 23:23:17 +0500

    Extend noteReducer: Add TOGGLE_IMPORTANCE action

--- counter-app/src/reducers/noteReducers.js
+++ counter-app/src/reducers/noteReducers.js
@@ -3,6 +3,17 @@ const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return state.concat(action.payload)
+    case 'TOGGLE_IMPORTANCE': {
+      const noteId = action.payload.id
+      const noteToChange = state.find(note => note.id === noteId)
+
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+
+      return state.map(note => note.id === noteId ? changedNote : note)
+    }
     default:
       return state
   }
