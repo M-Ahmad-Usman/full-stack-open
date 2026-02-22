a556c241f6032d69b914478a004b5086c46edf5d
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-02-22 15:45:44 +0500

    Implement actual app functionality using redux

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -4,16 +4,18 @@ import counterReducer from './reducers/counterReducer'
 
 const store = createStore(counterReducer)
 
+const dispatchAction = (type) => store.dispatch({ type })
+
 const App = () => {
   return (
     <div>
-      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={ () => dispatchAction('GOOD') }>good</button>
+      <button onClick={ () => dispatchAction('OK') }>ok</button>
+      <button onClick={ () => dispatchAction('BAD') }>bad</button>
+      <button onClick={ () => dispatchAction('RESET') }>reset stats</button>
+      <div>good { store.getState().good }</div>
+      <div>ok { store.getState().ok } </div>
+      <div>bad { store.getState().bad } </div>
     </div>
   )
 }
