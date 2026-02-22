178c09d9ebc2054d8a95a2770993428c927e45d3
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-02-22 19:42:28 +0500

    Convert counter-app to redux-notes.

---
+++ redux-notes/src/main.jsx
@@ -0,0 +1,63 @@
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+// Action creators
+const createNote = content => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = id => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+store.dispatch(createNote({ content: 'the app state is in redux store' }))
+store.dispatch(createNote({ content: 'state changes are made with actions' }))
+
+const App = () => {
+
+  const addNote = event => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+  const toggleImportance = id => store.dispatch(toggleImportanceOf(id))
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map(note => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => root.render(<App />)
+
+renderApp()
+store.subscribe(renderApp)
