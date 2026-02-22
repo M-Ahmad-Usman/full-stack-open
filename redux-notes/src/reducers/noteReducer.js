bb42b06a912403e431f71389cb6f30eefe97881a
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-02-22 19:55:05 +0500

    Move action creators to noteReducer

--- redux-notes/src/reducers/noteReducer.js
+++ redux-notes/src/reducers/noteReducer.js
@@ -4,18 +4,37 @@ const noteReducer = (state = [], action) => {
     case 'NEW_NOTE':
       return [...state, action.payload]
     case 'TOGGLE_IMPORTANCE': {
-      const noteId = action.payload.id
-      const noteToChange = state.find(note => note.id === noteId)
-
+      const id = action.payload.id
+      const noteToChange = state.find(n => n.id === id)
       const changedNote = {
         ...noteToChange,
         important: !noteToChange.important
       }
-
-      return state.map(note => note.id === noteId ? changedNote : note)
+      return state.map(note => (note.id !== id ? note : changedNote))
     }
     default:
       return state
+  }
+}
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
   }
 }
 
