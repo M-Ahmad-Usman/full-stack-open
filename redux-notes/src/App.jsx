1329ab391fea7e3032dab9bc97b0aab0079504f3
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-03-19 01:38:11 +0500

    Use the new VisibilityFilter.jsx component.

--- redux-notes/src/App.jsx
+++ redux-notes/src/App.jsx
@@ -1,10 +1,13 @@
 import NoteForm from './components/NoteForm'
+import VisibilityFilter from './components/VisibilityFilter'
 import Notes from './components/Notes'
 
 const App = () => {
+
   return (
     <div>
       <NoteForm />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
