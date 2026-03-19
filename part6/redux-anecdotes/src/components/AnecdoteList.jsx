d1ed135f6c63200e23829470eee51f46d44bbdde
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-03-20 02:49:56 +0500

    Render the anecdote list according to filter state

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,11 @@ import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
 
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes
+      .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+  })
+
   const dispatch = useDispatch()
 
   const vote = id => dispatch(voteAnecdote(id))
