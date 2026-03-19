592d2b1c2d9eaac43347289677c509665960f7a0
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-03-20 02:50:27 +0500

    Mount the filter to allow users to set filters.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import AnecdoteForm from './components/AnecdoteForm'
+import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList></AnecdoteList>
       <h2>create new</h2>
       <AnecdoteForm></AnecdoteForm>
