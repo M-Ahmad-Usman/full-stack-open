0f3a10235fc1df8cc0ba14cc874704caa5654e57
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-03-20 02:47:13 +0500

    Create filterReducer.js to manage filtering action
    - Create action type 'SET_FILTER' for the filter action.
    - Create action creator setFilter(value) to set the filter value in the state.

---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,19 @@
+
+const filterReducer = (state = '', action) => {
+  switch(action.type) {
+    case 'SET_FILTER':
+      return action.payload.filter
+    default:
+      return state
+  }
+}
+
+// Action Creators
+export const filterChange = filter => {
+  return {
+    type: 'SET_FILTER',
+    payload: { filter }
+  }
+}
+
+export default filterReducer
