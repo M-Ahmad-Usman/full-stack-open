f0d338306fd572f9acf6b78bd0b952bcc8aa8705
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-14 14:24:26 +0500

    Implement anecdote service in redux-notes.
    The anecdote service exposes getAll() function that returns all anecdotes from the server.

---
+++ part6/redux-anecdotes/src/services/anecdote.js
@@ -0,0 +1,14 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch anecdotes')
+  }
+
+  const anecdotes = await response.json()
+  return anecdotes
+}
+
+export default { getAll }
