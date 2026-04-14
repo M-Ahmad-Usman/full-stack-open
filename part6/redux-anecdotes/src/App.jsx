78adccdb025f06c4173e438f6071b08ccd358fdf
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-14 14:30:37 +0500

    Update App to fetch anecdotes in redux-anecdotes.
    The App.jsx component uses `useEffect` hook to fetch anecdotes from server during initial render.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import anecdoteService from './services/anecdote'
+
+import { setAnecdotes } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => dispatch(setAnecdotes( { anecdotes })))
+  }, [dispatch])
 
   return (
     <div>
