b45241c08ec455325929e96a3cb08f5ee2bc6c5b
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-06-12 18:00:11 +0500

    Enhance NoteForm UI using material-ui in notes-app

--- notes-app/client/src/components/NoteForm.jsx
+++ notes-app/client/src/components/NoteForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { TextField, Button } from '@mui/material'
 
 const NoteForm = ({ createNote }) => {
 
@@ -23,15 +24,14 @@ const NoteForm = ({ createNote }) => {
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
-        <label>
-          content
-          <input
-            value={newNote}
-            onChange={event => setNewNote(event.target.value)}
-            placeholder='write note content here'
-          />
-        </label>
-        <button type="submit">save</button>
+        <TextField
+          label='note content'
+          value={newNote}
+          onChange={event => setNewNote(event.target.value)}
+        />
+        <div>
+          <Button type='submit' variant='contained' style={{ marginTop: 10 }}>save</Button>
+        </div>
       </form>
     </div>
   )
